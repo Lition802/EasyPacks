a7660fc389bc148ee221a91990eaeda15927aaa6
Sbaoor <2023786106@qq.com>
2021-04-03 14:38:26 +0800

    添加项目文件。

---
+++ EasyPacks/json.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyPacks
+{
+    public class Header
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string description { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string uuid { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<int> version { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<int> min_engine_version { get; set; }
+    }
+
+    public class Modules
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string uuid { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<int> version { get; set; }
+    }
+
+    public class Manifest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int format_version { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Header header { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Modules> modules { get; set; }
+    }
+
+    public class Resource
+    {
+        public string pack_id { get; set; }
+        public List<int> version { get; set; }
+    }
+}
