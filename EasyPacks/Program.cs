ecbc25bd972ea83460ed33209835f6169a953257
Sbaoor <2959435045@qq.com>
2022-03-29 11:16:26 +0800

    update

--- EasyPacks/Program.cs
+++ EasyPacks/Program.cs
@@ -4,76 +4,40 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Microsoft.VisualBasic.Devices;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace EasyPacks
 {
     class Program
     {
-        static void Main()
+        public static void log(object o)
         {
-            Console.WriteLine(@"    ______                 ____             __       
-   / ____/___ ________  __/ __ \____ ______/ /_______
-  / __/ / __ `/ ___/ / / / /_/ / __ `/ ___/ //_/ ___/
- / /___/ /_/ (__  ) /_/ / ____/ /_/ / /__/ ,< (__  ) 
-/_____/\__,_/____/\__, /_/    \__,_/\___/_/|_/____/  
-                 /____/                             ");
-            DirectoryInfo bedir = new DirectoryInfo("behavior_packs");
-            DirectoryInfo redir = new DirectoryInfo("resource_packs");
-            DirectoryInfo[] dirinfos = bedir.GetDirectories();
-            DirectoryInfo[] dirinfos2 = redir.GetDirectories();
-            var behavior = new List<Resource>();
-            var resource = new List<Resource>();
-            if(bedir.Exists)
-            {
-                foreach (DirectoryInfo i in dirinfos)
-                {
-                    Console.WriteLine("[EasyPacks] Loading " + i.FullName + "\\manifest.json");
-                    if (File.Exists(i.FullName + "\\manifest.json"))
-                    {
-                        var json = JsonConvert.DeserializeObject<Manifest>(File.ReadAllText(i.FullName + "\\manifest.json"));
-                        var tmp = new Resource()
-                        {
-                            pack_id = json.header.uuid,
-                            version = json.header.version
-                        };
-                        behavior.Add(tmp);
-                    }
-                }
-            }           
-            if(redir.Exists)
-            {
-                foreach (DirectoryInfo i in dirinfos2)
-                {
-                    Console.WriteLine("[EasyPacks] Loading " + i.FullName + "\\manifest.json");
-                    if (File.Exists(i.FullName + "\\manifest.json"))
-                    {
-                        var json = JsonConvert.DeserializeObject<Manifest>(File.ReadAllText(i.FullName + "\\manifest.json"));
-                        var tmp = new Resource()
-                        {
-                            pack_id = json.header.uuid,
-                            version = json.header.version
-                        };
-                        resource.Add(tmp);
-                    }
-                }
-            }        
-            if(File.Exists("world_behavior_packs.json"))
-            {
-                Computer my = new Computer();
-                my.FileSystem.RenameFile("world_behavior_packs.json", "world_behavior_packs_old.json");
-            }
-            if (File.Exists("world_resource_packs.json"))
-            {
-                Computer my = new Computer();
-                my.FileSystem.RenameFile("world_resource_packs.json", "world_resource_packs_old.json");
-            }
-            File.WriteAllText("world_behavior_packs.json", JsonConvert.SerializeObject(behavior));
-            File.WriteAllText("world_resource_packs.json", JsonConvert.SerializeObject(resource));
-            Console.WriteLine("[EasyPacks] json file generated successfully！");
-            Console.WriteLine("Press any key to continue . . .");
-            Console.ReadKey();
+            Console.WriteLine($"[{DateTime.Now}] {o?.ToString()}");
         }
-    }
+        static void Main(string[] args)
+        {
+            log("EasyPacks v2.1.0");
+            Directory.CreateDirectory("behavior_packs");
+            Directory.CreateDirectory("resource_packs");
+            if(args.Length == 0)
+            {
+                helper.behavior_packs();
+                helper.resource_packs();
+                Console.WriteLine("Press any key to continue . . .");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            if(args[0].EndsWith("mcaddon") || args[0].EndsWith("mcpack") || args[0].EndsWith("zip"))
+            {
+                log($"unzip {args[0]}");
+                ZipHelper.UnZip(args[0],args[0]+".unzip");
+                Console.WriteLine("Press any key to continue . . .");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+    }     
 }
---
+++ EasyPacks/helper.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyPacks
+{
+    class helper
+    {
+        public static void behavior_packs()
+        {
+            if (Directory.Exists("behavior_packs"))
+            {
+                var bedir = new DirectoryInfo("behavior_packs").GetDirectories();
+                var beh = new JArray();
+                foreach (DirectoryInfo i in bedir)
+                {                    
+                    if (File.Exists(i.FullName + "\\manifest.json"))
+                    {
+                        var json = JsonConvert.DeserializeObject<Manifest>(File.ReadAllText(i.FullName + "\\manifest.json"));
+                        Program.log("Loading "+json.header.name);
+                        var j = new JObject
+                        {
+                            new JProperty("pack_id", json.header.uuid)
+                        };
+                        var ve = new JArray();
+                        foreach (var it in json.header.version)
+                        {
+                            ve.Add(it);
+                        }
+                        j.Add(new JProperty("version", ve));
+                        beh.Add(j);
+                    }
+                }
+                File.WriteAllText("world_behavior_packs.json", beh.ToString());
+                Program.log("behavior_packs done!");
+            }
+        }
+        public static void resource_packs()
+        {
+            if (Directory.Exists("resource_packs"))
+            {
+                var redir = new DirectoryInfo("resource_packs").GetDirectories();
+                var res = new JArray();
+                foreach (DirectoryInfo i in redir)
+                {
+                    //Console.WriteLine("[EasyPacks] Loading " + i.FullName + "\\manifest.json");
+                    if (File.Exists(i.FullName + "\\manifest.json"))
+                    {
+                        var json = JsonConvert.DeserializeObject<Manifest>(File.ReadAllText(i.FullName + "\\manifest.json"));
+                        Program.log("Loading " + json.header.name);
+                        var j = new JObject()
+                        {
+                            new JProperty("pack_id", json.header.uuid)
+                        };
+                        var ve = new JArray();
+                        foreach (var it in json.header.version)
+                        {
+                            ve.Add(it);
+                        }
+
+                        j.Add(new JProperty("version", ve));
+                        res.Add(j);
+                    }
+                }
+                File.WriteAllText("world_resource_packs.json", res.ToString());
+                Program.log("resource_packs done!");
+            }
+        }
+        public static void unzip_pack(string name)
+        {
+            try
+            {
+                FileInfo file = new FileInfo(name);
+                ZipHelper.UnZip(name,".packs\\"+file.Name+"\\");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+}
